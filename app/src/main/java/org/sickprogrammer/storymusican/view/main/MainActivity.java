aa0d54176c860d3d67dc5b18174b3d4caae17a7c
donguk <ldu2175@gmail.com>
2021-05-23 19:22:54 +0900

    init

---
+++ app/src/main/java/org/sickprogrammer/storymusican/view/main/MainActivity.java
@@ -0,0 +1,16 @@
+package org.sickprogrammer.storymusican.view.main;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import org.sickprogrammer.storymusican.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
